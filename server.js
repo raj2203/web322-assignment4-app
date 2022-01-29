db7c249ae898a1253bb707621eaf826d3b867cdc
Prince Jodhani <pdjodhani@myseneca.ca>
2022-01-29 15:26:44 +0530

    dat 2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
-var path = require("path");
 var app = express();
+
+var path = require("path");
+var blogService = require("blog-service.js");
 
 var HTTP_PORT = process.env.PORT || 8080;
 app.use(express.static('public'));
