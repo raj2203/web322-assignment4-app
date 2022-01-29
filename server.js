938fe301ef4768c12be005fa4e9614815aebfae7
Prince Jodhani <pdjodhani@myseneca.ca>
2022-01-29 15:45:52 +0530

    updated routes

--- server.js
+++ server.js
@@ -18,3 +18,14 @@ app.get("/", function(req,res){
 app.get("/about", function(req,res){
     res.sendFile(path.join(__dirname, "/views/about.html"));
 });
+
+
+app.get("/blog", function(req,res){
+    res.send("in blog");
+});
+app.get("/posts", function(req,res){
+    res.send("inside posts");
+});
+app.get("/categories", function(req,res){
+    res.send("inside categories");
+});
