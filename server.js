38d5cf4a7b7a5eb347e71f3b1e9a1eb28c6db627
Prince Jodhani <pdjodhani@myseneca.ca>
2022-02-17 21:13:57 -0500

    updated all

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
-* WEB322 – Assignment 2
+* WEB322 – Assignment 3
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
 *
-* Name: Prince Jodhani   Student ID: 149455206   Date: 04/02/2022
+* Name: Prince Jodhani   Student ID: 149455206   Date: 17/02/2022
 *
-* Online (Heroku) URL: https://web322-assignment2-prince.herokuapp.com/
+* Online (Heroku) URL: https://web322-assignment3-prince.herokuapp.com/
 *
 ********************************************************************************/
 
